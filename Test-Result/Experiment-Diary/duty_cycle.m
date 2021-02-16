120a35f93a1c506eded15a3e89334ae0e0fd6a3f
EnesAyaz <enes.ayaz.1996@gmail.com>
2021-02-16 08:59:59 +0300

    added a experiment preparing documents

---
+++ Test-Result/Experiment-Diary/duty_cycle.m
@@ -0,0 +1,33 @@
+D=0:0.05:1;
+Co=(2*D-1);
+
+k=1;
+Ck= 4*D.*(sin(k*pi*D)./(k*pi*D)).*exp(-1i*k*pi.*D);
+abs(Ck)
+plot(D,Co,'LineWidth',3)
+hold on;
+plot(D,abs(Ck),'LineWidth',3)
+hold on;
+
+%%
+k=2;
+Ck= 4*D.*(sin(k*pi*D)./(k*pi*D)).*exp(-1i*k*pi.*D);
+abs(Ck)
+plot(D,abs(Ck),'LineWidth',3)
+hold on;
+
+k=3;
+Ck= 4*D.*(sin(k*pi*D)./(k*pi*D)).*exp(-1i*k*pi.*D);
+abs(Ck)
+plot(D,abs(Ck),'LineWidth',3)
+hold on;
+
+k=4;
+Ck= 4*D.*(sin(k*pi*D)./(k*pi*D)).*exp(-1i*k*pi.*D);
+abs(Ck)
+plot(D,abs(Ck),'LineWidth',3)
+hold on;
+xlabel('Duty Cycle');
+ylabel('Voltage Gain')
+legend('DC','1st','2nd','3rd','4th')
+set(gca,'Fontsize',16)
