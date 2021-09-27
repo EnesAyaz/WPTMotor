ec5249b61ee30aeecc116a006e7e6519d912b335
EnesAyaz <enes.ayaz.1996@gmail.com>
2021-09-27 13:43:06 +0300

    calorimeter results, the loss is lower than expected.
    we think that Low current switch

---
+++ Test-Result/2021.09.24/calorimeter_plotter.m
@@ -0,0 +1,31 @@
+load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulIPT.mat');
+figure();
+plot(time_step, power_est_mva);
+%%
+load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulMOTOR.mat')
+figure();
+plot(time_step, power_est_mva);
+
+%%
+load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulMOTORIPT.mat')
+figure();
+plot(time_step, power_est_mva);
+
+%%
+load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulIPTfandeneme.mat')
+ figure();
+plot(time_step, power_est_mva);
+
+%%
+ load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulfandeneme.mat')
+ figure();
+plot(time_step, power_est_mva);
+
+%%
+load('G:\My Drive\WPT-MOTOR\Test-Result\2021.09.24\24eylulMOTORIPTfandeneme.mat')
+ figure();
+plot(time_step, power_est_mva);
+
+% Pin 68 468 536
+
+
