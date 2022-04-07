9fff940b447df6244e5b9f291176ddfa5fd7b27a
EnesAyaz <enes.ayaz.1996@gmail.com>
2022-04-07 13:27:26 +0300

    For synchronous machine, the ansys model, matlab and ltspice simulations
    also mechanic drawings

--- Synhronous motor/System-Design/design2.m
+++ Synhronous motor/System-Design/design2.m
@@ -2,28 +2,30 @@ clear all;
 clc; 
 % The code is used for 1p-1s design series-series WPT
 % Input and power, Output power and load, Quality factor of seconder side
+% The design is made for constant voltage point
 %% FILL INFORMATION BELOW
 Vdc=400; % V (dc)
 G_inv=0.45;
 Vin=G_inv*Vdc; %V  (rms) 
 P_o=225; %W   
-Qs=4; % unitless
-f=50e3; %Hz
+Qs=2; % unitless
+fh=50e3; %Hz
 % f=80e3; %Hz
-k=0.45; % chosen coupling factor
+k=0.6; % chosen coupling factor
 % k=0.25; % chosen coupling factor
 kc= (1/Qs)*sqrt(1-1/(4*Qs^2));
-w=2*pi*f; %rad/sec
+wh=2*pi*fh; %rad/sec
 % Calculation Series-compensated paralel DC BUS
 R_L= 0.81; %% single module R_L
-Vout=15; %V (rms)
 Vout=13.5; %V (rms)
 
 Ip_rms= P_o/Vin; % primary current assuming eff=1
 Is_rms=(P_o/Vout);
-M=Is_rms*R_L/(w*Ip_rms); %calculated mutual inductance 
-Ls=Qs*R_L/w;    % secondary coil inductance
-Lp=M^2/(k^2*Ls);  %primary inductance
+Ls=Qs*R_L/wh;    % secondary coil inductance
+G=Vout/Vin;
+Lp=Ls/(G^2);
+M=k*sqrt(Lp*Ls);
+w= wh*sqrt(1-k);
 Cp=1/(w^2*Lp);  %primary compansation
 Cs=1/(w^2*Ls);  %secondary compansation
 %% Load changes
@@ -51,16 +53,16 @@ rs= 0.03; % ohm
 Lp_p=Lp-M; % Henry as actually Lp-m Lp is 120u
 Ls_p=Ls-M; % Henry as actually Ls-m Ls is 120u
 f=logspace(4.4,5,200);
-w=2*pi*f;
+wh=2*pi*f;
 %%
-Z1= 1i.*w*Ls_p +R_L- 1i./(w*Cs);
-Z2=1i.*w*M;
-Z3= 1i.*w*Lp_p+rp+rs-1i./(w*Cp);
+Z1= 1i.*wh*Ls_p +R_L- 1i./(wh*Cs);
+Z2=1i.*wh*M;
+Z3= 1i.*wh*Lp_p+rp+rs-1i./(wh*Cp);
 Z12= ((Z1.*Z2)./(Z1+Z2));
 Z123=Z12+Z3;
 %%
 V1= (Z12./Z123)*Vin; 
-V_load= (R_L./(1i*w*Ls_p+R_L-1i./(w*Cs))).*V1;
+V_load= (R_L./(1i*wh*Ls_p+R_L-1i./(wh*Cs))).*V1;
 Vo_angle= angle(V_load)*180/pi;
 Vo_mag= abs(V_load);
 Iin=Vin./Z123;
@@ -92,7 +94,7 @@ grid on;
 set(gca,'FontSize',10);
 %%
 
-Vcp=Iin./(1i*w*Cp);
+Vcp=Iin./(1i*wh*Cp);
 figure;
 hold on;
 plot(f,abs(Vcp),'Linewidth',2);
---
+++ Synhronous motor/System-Design/design_SN.m
@@ -0,0 +1,29 @@
+clc;
+k=0.8;
+Vdc=400; % V (dc)
+G_inv=0.45;
+Vin=G_inv*Vdc; %V  (rms) 
+f=50e3; %Hz
+w=2*pi*f; %rad/sec
+% Calculation Series-compensated paralel DC BUS
+R_L= 0.81; %% single module R_L
+Vout=13.5; %V (rms)
+
+n=Vin/Vout;
+
+LpdivideLs=(n/k)^2;
+
+Q=2.8;
+Ls=Q*R_L/w;
+Lp=LpdivideLs*Ls;
+
+Lr=(1-k^2)*Lp;
+
+Cr=1/(w^2*Lr);
+
+M=k*sqrt(Lp*Ls);
+
+Lp*1e6
+Ls*1e6
+Cr*1e9
+M*1e6
